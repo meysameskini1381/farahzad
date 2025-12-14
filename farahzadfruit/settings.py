f770964b7e089db5bda412845c7e2b5b0891b6b1
meysameskini1381 <meysameskini.123@gmail.com>
2025-12-14 01:39:28 -0800

    edit template index

--- farahzadfruit/settings.py
+++ farahzadfruit/settings.py
@@ -181,8 +181,9 @@ CKEDITOR_5_CONFIGS = {
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
 
-]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
